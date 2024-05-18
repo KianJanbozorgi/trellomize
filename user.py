26300a07756e0661201403cf9ef4aa4ba9521779
Asa <asabayatnaderi2005@gmail.com>
2024-05-19 01:51:04 +0330

    Refactor User class using File class

--- user.py
+++ user.py
@@ -1,10 +1,13 @@
 from pathlib import Path
-import csv
-from menu import Menu
 import re
+from util import File
 
-users_file = Path("info/users.csv")
 info_dir = Path("info")
+users = Path("info/users.csv")
+users_file = File("info/users.csv")
+project_file = File("info/project.csv")
+
+
 Invalid_email = """Invalid email address.
 """
 invalid_username = """A valid username can include letters(uppercase and lowercase), numbers, '_', '.', '-'.
@@ -26,10 +29,8 @@ class User:
     def make_dir_or_file(self):
         if not info_dir.exists():
             info_dir.mkdir()
-        if not users_file.exists():
-            with open('info/users.csv', 'w', newline="") as user_info:
-                writer = csv.writer(user_info)
-                writer.writerow(["email", "username", "password", "account"])
+        if not users.exists():
+            users_file.write(["email", "username", "password", "account"])
 
     def check_email(self):
         pattern = r"^[a-zA-Z0-9_. +-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"
@@ -54,53 +55,44 @@ class User:
         self.password = input("-Please enter a strong password: ")
         self.check_password()
         self.make_dir_or_file()
-        with open("info/users.csv", "r") as users_info:
-            reader = csv.reader(users_info)
-            for email, username, password, account in reader:
-                if email == self.email:
-                    raise Exception("Duplicate email")
-                if username == self.username:
-                    raise Exception("Duplicate username")
-        self.write_info()
+        reader = users_file.read()
+        for email, username, password, account in reader:
+            if email == self.email:
+                raise Exception("Duplicate email")
+            if username == self.username:
+                raise Exception("Duplicate username")
+        users_file.append([self.email, self.username,
+                          self.password, self.account])
         return self.username
-
-    def write_info(self):
-        with open("info/users.csv", "a", newline="") as users_info:
-            writer = csv.writer(users_info)
-            writer.writerow([self.email, self.username,
-                            self.password, self.account])
 
     def log_in(self):
         self.username = input("-Username: ")
         self.password = input("-Password: ")
         self.make_dir_or_file()
-        with open("info/users.csv", "r") as users_info:
-            reader = csv.reader(users_info)
-            for email, username, password, account in reader:
-                if username == self.username and password == self.password:
-                    if account == "active":
-                        break
-                    else:
-                        raise Exception("Your account is deactive.")
-            else:
-                raise Exception("Wrong username or password.")
+        reader = users_file.read()
+        for email, username, password, account in reader:
+            if username == self.username and password == self.password:
+                if account == "active":
+                    break
+                else:
+                    raise Exception("Your account is deactive.")
+        else:
+            raise Exception("Wrong username or password.")
         return self.username
 
     def get_leader_projects(self):
         leader_projects = []
-        with open("info/project.csv", "r") as project_info:
-            reader = csv.reader(project_info)
-            for info in reader:
-                if self.username == info[4]:
-                    leader_projects.append(info)
+        reader = project_file.read()
+        for info in reader:
+            if self.username == info[4]:
+                leader_projects.append(info)
         return leader_projects
 
     def get_user_projects(self):
         user_projects = []
-        with open("info/project.csv", "r") as project_info:
-            reader = csv.reader(project_info)
-            for info in reader:
-                for member in info[5:]:
-                    if self.username == member:
-                        user_projects.append(info)
+        reader = project_file.read()
+        for info in reader:
+            for member in info[5:]:
+                if self.username == member:
+                    user_projects.append(info)
         return user_projects
