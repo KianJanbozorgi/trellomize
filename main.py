c6544098bb77d860c36601feae95606055f2c414
Asa <asabayatnaderi2005@gmail.com>
2024-05-19 19:56:08 +0330

    Update main

--- main.py
+++ main.py
@@ -1,18 +1,54 @@
 import pandas as pd
 import random
 import project
+from menu import *
+from user import *
 
-def authorization(x,y):
-    df = pd.read_csv('users.csv')
-    m = list(df['password'].loc[df['username'] == x])
-    if len(m) == 0:
-        return "username not found"
-    else:
-        if m[0] == y :
-            return "you loged in successfully"
-        else:
-            return "password not found"
 
+def main():
+    main_menu = Menu(["What do you want to do?", "Sign Up", "Log In", "Exit"])
+
+    console.clear()
+    main_menu.display()
+    user = User()
+    proj = project.Project()
+    if main_menu.selected_option == "Sign Up":
+        try:
+            project.username = user.sign_up()
+            print("signed up successfully")
+            curses.napms(1000)
+            console.clear()
+            proj.project_menu(user)
+        except Exception as ex:
+            print(ex)
+
+    elif main_menu.selected_option == "Log In":
+        try:
+            project.username = user.log_in()
+            print("Log in successfully")
+            curses.napms(1000)
+            console.clear()
+            proj.project_menu(user)
+        except Exception as ex:
+            print(ex)
+
+    elif main_menu.selected_option == "Exit":
+        pass
+
+
+if __name__ == "__main__":
+    main()
+
+# def authorization(x,y):
+#     df = pd.read_csv('users.csv')
+#     m = list(df['password'].loc[df['username'] == x])
+#     if len(m) == 0:
+#         return "username not found"
+#     else:
+#         if m[0] == y :
+#             return "you loged in successfully"
+#         else:
+#             return "password not found"
 
 
 # x = input('username :')
@@ -24,11 +60,11 @@ def authorization(x,y):
 # """)
 # f.close()
 
-x = input('username :')
-y = input('password :')
+# x = input('username :')
+# y = input('password :')
 
-a = project.Project()
-b = project.Duty()
-a.add_duties(b)
-print(a.duties[0].status)
-print(authorization(x,y))
+# a = project.Project()
+# b = project.Duty()
+# a.add_duties(b)
+# print(a.duties[0].status)
+# print(authorization(x,y))
